f62170ac009b9208c626ab1a40b5b84b52031f89
VICENTINI CAMILA <111746@tecnicatura.frc.utn.edu.ar>
2026-06-03 14:10:37 -0300

    proyect init

---
+++ backend/src/test/java/nutricentro/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nutricentro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

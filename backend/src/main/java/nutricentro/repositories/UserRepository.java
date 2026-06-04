45345def40a65789e7d4b4e3da812410a97ed979
VICENTINI CAMILA <111746@tecnicatura.frc.utn.edu.ar>
2026-06-04 20:19:31 -0300

    repositories

---
+++ backend/src/main/java/nutricentro/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package nutricentro.repositories;
+
+import nutricentro.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsernameIgnoreCase(String username);
+    Optional<UserEntity> findByEmailIgnoreCase(String email);
+    boolean existsByUsernameIgnoreCase(String username);
+    boolean existsByEmailIgnoreCase(String email);
+}

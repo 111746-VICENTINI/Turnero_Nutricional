c4e1251fcd327d114777f298d621c4ba462e395a
VICENTINI CAMILA <111746@tecnicatura.frc.utn.edu.ar>
2026-06-04 20:21:07 -0300

    role dto

---
+++ backend/src/main/java/nutricentro/dtos/roles/RoleResponseDTO.java
@@ -0,0 +1,18 @@
+package nutricentro.dtos.roles;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoleResponseDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Integer hierarchy;
+}
+

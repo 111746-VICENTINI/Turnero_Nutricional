3f62d57c575abcab16c3089f2ac6cfb9c2f4ccab
VICENTINI CAMILA <111746@tecnicatura.frc.utn.edu.ar>
2026-06-04 20:19:09 -0300

    update user dto

---
+++ backend/src/main/java/nutricentro/dtos/users/UserResponseDTO.java
@@ -0,0 +1,18 @@
+package nutricentro.dtos.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDTO {
+	private Long id;
+	private String username;
+	private String email;
+	private Boolean isActive;
+	private Set<String> roles;
+}

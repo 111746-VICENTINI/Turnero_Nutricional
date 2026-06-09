497baba644fe26e820ed050c0e3f62b3ed48a8ea
VICENTINI CAMILA <111746@tecnicatura.frc.utn.edu.ar>
2026-06-09 19:48:18 -0300

    add patient service

---
+++ frontend/nutricentro/src/app/components/patients/services/patient-service.ts
@@ -0,0 +1,40 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../enviroment/enviroment';
+import {PatientRequestDTO, PatientResponseDTO, PatientUpdateDTO} from '../models/Patient-model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PatientService {
+  private http = inject(HttpClient);
+  private readonly apiUrl = `${environment.apiUrl}/v1/patient`;
+
+  createPatient(request: PatientRequestDTO) {
+    return this.http.post<PatientResponseDTO>(
+      `${this.apiUrl}/create`, request);
+  }
+
+  getAllPatients() {
+    return this.http.get<PatientResponseDTO[]>(
+      `${this.apiUrl}/all`
+    );
+  }
+
+  getByIdPatient(id: number) {
+    return this.http.get<PatientResponseDTO>(
+      `${this.apiUrl}/${id}`
+    );
+  }
+
+  updatePatient(id: number, request: PatientUpdateDTO) {
+    return this.http.put<PatientResponseDTO>(
+      `${this.apiUrl}/${id}`, request
+    );
+  }
+
+  deletePatient(id: number) {
+    return this.http.patch<void>(`${this.apiUrl}/${id}`, {});
+  }
+
+}

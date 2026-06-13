0c2cbeaabfeb0f24a926cf1ea31ebda24cf67883
VICENTINI CAMILA <111746@tecnicatura.frc.utn.edu.ar>
2026-06-13 20:08:38 -0300

    changes in drawer

--- frontend/nutricentro/src/app/layout/drawer/drawer.ts
+++ frontend/nutricentro/src/app/layout/drawer/drawer.ts
@@ -1,28 +1,31 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { DrawerModule } from 'primeng/drawer';
 import { RippleModule } from 'primeng/ripple';
 import { StyleClassModule } from 'primeng/styleclass';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NavigationEnd, Router, RouterLink, RouterOutlet} from '@angular/router';
 import {AuthService} from '../../core/services/auth-service';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-drawer',
   standalone: true,
   imports: [CommonModule, RouterOutlet, AvatarModule,
     RouterLink,
-    RouterLinkActive,
     ButtonModule, DrawerModule, RippleModule, StyleClassModule, NgOptimizedImage],
   templateUrl: './drawer.html',
   styleUrl: './drawer.css',
 })
-export class Drawer {
+export class Drawer implements OnInit{
   visible = false;
   usersMenu = false;
+  patientsMenu = false;
+  professionalsMenu = false;
 
   authService = inject(AuthService);
+  router = inject(Router);
   roles = this.authService.roles;
   actualUser = this.authService.getCurrentUser();
 
@@ -30,12 +33,20 @@ export class Drawer {
   isSecretary() { return this.roles().includes('SECRETARY'); }
   isProfessional() { return this.roles().includes('PROFESSIONAL'); }
 
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => {
+        this.visible = false;
+      });
+  }
+
   logout() {
     this.visible = false;
     this.authService.logout();
   }
 
   getFullName():string {
-    return `${this.actualUser?.username || 'usuario'}`;
+    return `${this.actualUser?.username}`;
   }
 }

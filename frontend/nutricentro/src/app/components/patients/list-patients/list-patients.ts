65b82d07c7393ed742fc4509cf54b7042165f5c3
VICENTINI CAMILA <111746@tecnicatura.frc.utn.edu.ar>
2026-06-15 15:26:39 -0300

    implementacion de tabla generica en listado de usuarios

--- frontend/nutricentro/src/app/components/patients/list-patients/list-patients.ts
+++ frontend/nutricentro/src/app/components/patients/list-patients/list-patients.ts
@@ -1,18 +1,76 @@
 import {Component, inject} from '@angular/core';
-import {AuthService} from '../../../core/services/auth-service';
+import {Button} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {TableGeneric} from '../../../shared/components/table-generic/table-generic';
+import {PatientResponseDTO} from '../models/Patient-model';
+import {PatientService} from '../services/patient-service';
+import {MessageService} from 'primeng/api';
+import {Router} from '@angular/router';
+import {TableActionConfig, TableColumnConfig} from '../../../shared/components/table-generic/model/table-model';
 
 @Component({
   selector: 'app-list-patients',
   standalone: true,
-  imports: [],
+  imports: [
+    Button,
+    TableModule,
+    TableGeneric
+  ],
   templateUrl: './list-patients.html',
   styleUrl: './list-patients.css',
 })
 export class ListPatients {
-  authService = inject(AuthService);
-  roles = this.authService.roles;
+    patients: PatientResponseDTO[] = [];
 
-  isAdmin() {
-    return this.roles().includes('ADMIN');
+    private patientService = inject(PatientService);
+    private messageService = inject(MessageService);
+    private router = inject(Router);
+
+  columns: TableColumnConfig<PatientResponseDTO>[] = [
+    { field: 'lastName', header: 'Apellido' },
+    { field: 'firstName', header: 'Nombre' },
+    { field: 'age', header: 'Edad' },
+    { field: 'email', header: 'Email' },
+    { field: 'status', header: 'Activo' }
+  ];
+
+  actions: TableActionConfig<PatientResponseDTO>[] = [
+    { field: 'view', label: 'Ver', icon: 'pi pi-eye', severity: 'secondary' },
+    { field: 'edit', label: 'Editar', icon: 'pi pi-pencil', severity: 'info' },
+    { field: 'delete', label: 'Eliminar', icon: 'pi pi-trash', severity: 'danger' },
+  ];
+
+  ngOnInit(): void {
+    this.loadPatients();
+  }
+
+  loadPatients(): void {
+    this.patientService.getAllPatients().subscribe({
+      next: (patients) => this.patients = patients,
+      error: () => this.messageService.add({ severity: 'error', summary: 'Error', detail: 'No se pudieron cargar los pacientes' })
+    });
+  }
+
+  createPatient(): void {
+    this.router.navigate(['/patient/create']);
+  }
+
+  viewPatient(patient: PatientResponseDTO): void {
+    this.router.navigate(['/patient', patient.id]);
+  }
+
+  deletePatient(patient: PatientResponseDTO): void {
+    this.patientService.deletePatient(Number(patient.id)).subscribe({
+      next: () =>
+      {
+        this.loadPatients();
+        this.messageService.add({ severity: 'success', summary: 'Exito', detail: 'Paciente eliminado' });
+      },
+      error: () => this.messageService.add({ severity: 'error', summary: 'Error', detail: 'No se pudo eliminar el paciente' })
+    })
+  }
+
+  editPatient(patient: PatientResponseDTO){
+    this.router.navigate(['/patient', patient.id ,'edit']);
   }
 }

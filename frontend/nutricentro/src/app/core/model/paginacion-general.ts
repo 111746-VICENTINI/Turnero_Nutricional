5b23acedc3827d55049e9e46c2e9eb47f06a8a96
VICENTINI CAMILA <111746@tecnicatura.frc.utn.edu.ar>
2026-06-15 20:58:55 -0300

    add new pagination general

---
+++ frontend/nutricentro/src/app/core/model/paginacion-general.ts
@@ -0,0 +1,15 @@
+export interface PageResponse<T> {
+  content: T[];
+  number: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
+}
+
+export interface TableState {
+  first: number;
+  rows: number;
+  sortField?: string;
+  sortOrder?: number;
+  search?: string;
+}

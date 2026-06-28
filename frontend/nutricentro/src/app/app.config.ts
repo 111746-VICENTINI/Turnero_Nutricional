039629a8ae83b6563d6b2ea4a2d9b74f655fc23a
VICENTINI CAMILA <111746@tecnicatura.frc.utn.edu.ar>
2026-06-28 17:39:03 -0300

    update app config.ts

--- frontend/nutricentro/src/app/app.config.ts
+++ frontend/nutricentro/src/app/app.config.ts
@@ -21,6 +21,18 @@ export const appConfig: ApplicationConfig = {
         options: {
           darkModeSelector: '.app-dark'
         }
+      },
+      translation: {
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+        dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
+          'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthNamesShort: ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'],
+        today: 'Hoy',
+        clear: 'Limpiar',
+        firstDayOfWeek: 1,
+        dateFormat: 'dd/mm/yy'
       }
     })
   ]

e8f170335677af3d944be4cc4e23c263c3e0e4c4
VICENTINI CAMILA <111746@tecnicatura.frc.utn.edu.ar>
2026-07-05 15:25:37 -0300

    update tabs in history clinical

--- frontend/nutricentro/src/app/components/history-clinical/history-tabs/history-tabs.ts
+++ frontend/nutricentro/src/app/components/history-clinical/history-tabs/history-tabs.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from 'primeng/tabs';
-import { PatientResponseDTO } from '../../patients/models/patient-model';
-import { MedicalHistoryResponseDTO } from '../models/history-clinical-model';
+import {MedicalHistoryResponseDTO, NutritionalDataDTO} from '../models/history-clinical-model';
 import { TabAnthropometry } from '../tabs/tab-anthropometry/tab-anthropometry';
 import { TabClinicalData } from '../tabs/tab-clinical-data/tab-clinical-data';
 import { TabConsultations } from '../tabs/tab-consultations/tab-consultations';
@@ -9,6 +8,7 @@ import { TabLaboratory } from '../tabs/tab-laboratory/tab-laboratory';
 import { TabNutritionalData } from '../tabs/tab-nutritional-data/tab-nutritional-data';
 import { TabPatientSummary } from '../tabs/tab-patient-summary/tab-patient-summary';
 import { TabPlans } from '../tabs/tab-plans/tab-plans';
+import { TabFiles } from '../tabs/tab-files/tab-files';
 
 @Component({
   selector: 'app-history-tabs',
@@ -25,17 +25,25 @@ import { TabPlans } from '../tabs/tab-plans/tab-plans';
     TabAnthropometry,
     TabPlans,
     TabConsultations,
+    TabFiles,
   ],
   templateUrl: './history-tabs.html',
   styleUrl: './history-tabs.css',
 })
 export class HistoryTabs {
-  @Input({ required: true }) patient!: PatientResponseDTO;
+  @Input() nutritionData?: NutritionalDataDTO;
   @Input({ required: true }) history!: MedicalHistoryResponseDTO;
+  @Input() activeTab = 'summary';
 
   @Output() historyChanged = new EventEmitter<void>();
+  @Output() activeTabChanged = new EventEmitter<string>();
 
   notifyChange(): void {
     this.historyChanged.emit();
+  }
+
+  selectTab(tab: string): void {
+    this.activeTab = tab;
+    this.activeTabChanged.emit(tab);
   }
 }
